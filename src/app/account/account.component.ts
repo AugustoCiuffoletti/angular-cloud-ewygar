b20e13d86a0ad6cdfdd6839fcbf95de6ee09b3f2
AugustoCiuffoletti <augusto@di.unipi.it>
2022-04-22 17:05:47 +0200

    upload incompleta

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -23,19 +23,76 @@ export class AccountComponent implements OnInit {
     const xhttp = new XMLHttpRequest();
     let message: string;
     console.log(query);
-    var obs = this.db.remove(query);
+    let obs = this.db.remove(query);
     obs.subscribe({
       next: (responseText: string) => {
         console.log(responseText);
         document.getElementById('removeOutput').innerHTML = responseText;
-        (<HTMLInputElement> document.getElementById('removeQuery')).value='';
+        (<HTMLInputElement>document.getElementById('removeQuery')).value = '';
       },
       error: (e: Error) => {
         console.error(e);
-        document.getElementById('removeOutput').innerHTML =  e.message;
-        (<HTMLInputElement> document.getElementById('removeQuery')).value='';
+        document.getElementById('removeOutput').innerHTML = e.message;
+        (<HTMLInputElement>document.getElementById('removeQuery')).value = '';
         throw e.message;
-      }
+      },
     });
+  }
+
+  upload(event) {
+    let file: File = event.target.files[0];
+    let filename: string = file.name;
+    let source: string = filename.split('.')[0];
+    let date: string, description: string;
+    console.log(filename);
+    try {
+      [date, description] = this.checkfilename(filename);
+    } catch (e) {
+      console.error('Non compliant filename: ' + e);
+      throw e;
+    }
+    console.log(date);
+    console.log(description);
+    let obs = this.db.stat(source);
+    obs.subscribe({
+      next: (responseText: string) => {
+        let stat = JSON.parse(responseText);
+        console.log(stat.n);
+        (<HTMLInputElement>document.getElementById('uploadQuery')).value = '';
+        if (stat.n != 0) {
+          document.getElementById(
+            'uploadOutput'
+          ).innerHTML = `A source named ${source} has been already uploaded`;
+          throw `${source} is duplicate`;
+        }
+        let obs = this.db.upload(file, source);
+        obs.subscribe({
+          next: (responseText: string) => {
+            console.log('Uploaded');
+          },
+          error: (e: Error) => {
+            console.log('Error');
+          },
+        });
+      },
+      error: (e: Error) => {
+        console.error(e);
+        document.getElementById('removeOutput').innerHTML = e.message;
+        (<HTMLInputElement>document.getElementById('removeQuery')).value = '';
+        throw e.message;
+      },
+    });
+    document.getElementById('uploadOutput').innerHTML = 'Loading';
+  }
+
+  private checkfilename(fn: string) {
+    let parts = fn.split('.');
+    if (parts.length !== 2) throw 'no dots in the description';
+    if (parts[1] !== 'geojson') throw 'extension MUST be "geojson"';
+    if (parts[0].length < 5 || parts[0].length > 30)
+      throw 'description length in [5..30]';
+    if (!parts[0].match(/^[0-9]{8}-[0-9a-zA-Z]+$/))
+      throw 'Description must be in format "yyyymmdd-xxxxxxxxxxx"';
+    return parts[0].split('-');
   }
 }
--- src/app/account/mongodb.service.ts
+++ src/app/account/mongodb.service.ts
@@ -7,6 +7,7 @@ import { HttpHeaders } from '@angular/common/http';
 export class MongoDB {
   key: string;
   removeSecret = 'czYZJvY2';
+  uploadSecret = 'arazHX6V';
   URL: string =
     'https://data.mongodb-api.com/app/underlandscape-app-fwkpt/endpoint';
 
@@ -14,13 +15,46 @@ export class MongoDB {
     this.key = '9cf84c28';
   }
 
+// removes documents based on query string
   public remove(query: string): Observable<Object> {
     console.log(query);
-    const headers= new HttpHeaders().set('content-type', 'application/json');
-    return this.http.get(`${this.URL}/remove?secret=${this.removeSecret}&source=${query}`, { 'headers': headers });
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.http.get(
+      `${this.URL}/remove?secret=${this.removeSecret}&source=${query}`,
+      { headers: headers }
+    );
+  }
+// returns object with statistics about matched documents
+  public stat(query: string): Observable<Object> {
+    console.log(query);
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.http.get(
+      `${this.URL}/stat?source=${query}`,
+      { headers: headers }
+    );
   }
 
-  public postvalue(data: string, key: string): Observable<Object> {
-    return this.http.post(this.URL + '/set?key=' + key, data);
+  public upload(file: File, source: string): Observable<Object> {
+    let fileReader = new FileReader();
+    fileReader.readAsText(file);
+    fileReader.onload = function () {
+      try {
+        let data = JSON.parse(fileReader.result);
+        if (data['type'] !== 'FeatureCollection') {
+          throw 'Not a FeatureCollection';
+        }
+        let n = 0;
+        for (let feature of data.features) {
+          feature.source = file.name.split('.')[0];
+          feature.serial = n + 1;
+          //        console.log(JSON.stringify(feature));
+          n = n + 1;
+        }
+    const formData = new FormData();
+    formData.append("features", filename);
+    return this.http.post(
+      `${this.URL}/upload_geojson?secret=arazHX6V`, 
+      formData
+    );
   }
 }
